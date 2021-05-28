d0093437b7f9179be912cc20a3126517d1edc0cf
pradnyalgandhi <pradnyalgandhi28@gmail.com>
2021-05-28 21:18:03 +0530

    Updated dashboard.dart

--- dumbbell_club/lib/screens/dashboard.dart
+++ dumbbell_club/lib/screens/dashboard.dart
@@ -8,26 +8,40 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Monday, June1"),
-                    Image.asset('images/user.png')
-                  ],
+    final height = MediaQuery.of(context).size.height;
+    return ClipRRect(
+      borderRadius: BorderRadius.vertical(
+        bottom: Radius.circular(40),
+      ),
+      child: Container(
+        padding: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
+        width: MediaQuery.of(context).size.width,
+        height: height * 0.3,
+        color: Colors.black87,
+        child: Column(
+          children: [
+            ListTile(
+                title: Text(
+                  "Monday, June 1",
+                  style: TextStyle(
+                      color: Colors.white54, fontWeight: FontWeight.w300),
                 ),
-                SizedBox(
-                  height: 20.0,
+                subtitle: Text(
+                  "Hello Jon Snow",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 20.0,
+                  ),
                 ),
-                Container(
-                  child: Text("weekly data"),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.rectangle, color: Colors.black87),
-                )
-              ],
-            ), 
+                trailing: CircleAvatar(
+                  backgroundImage: ExactAssetImage(
+                    'images/profile.JPG',
+                  ),
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
--- dumbbell_club/lib/screens/home.dart
+++ dumbbell_club/lib/screens/home.dart
@@ -1,8 +1,6 @@
-import 'package:dumbbell_club/constant.dart';
 import 'package:dumbbell_club/screens/profile.dart';
 import 'package:dumbbell_club/screens/workout_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'dashboard.dart';
 import 'nutrition_screen.dart';
@@ -27,159 +25,150 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.only(
-            left: 20.0,
-            top: 50.0,
-            right: 20.0,
-          ),
-          child: SingleChildScrollView(child: PageStorage(
-            child: currentScreen,
-            bucket: bucket),
-          ),
+    return Scaffold(
+      body: SingleChildScrollView(child: PageStorage(
+        child: currentScreen,
+        bucket: bucket),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.amber.shade700,
+        onPressed: () {},
+        child: Text(
+          "GO",
+          style: TextStyle(
+              fontWeight: FontWeight.w900, fontStyle: FontStyle.italic),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.amber.shade700,
-          onPressed: () {},
-          child: Text(
-            "GO",
-            style: TextStyle(
-                fontWeight: FontWeight.w900, fontStyle: FontStyle.italic),
+        elevation: 2.0,
+      ),
+      bottomNavigationBar: BottomAppBar(
+    shape: CircularNotchedRectangle(),
+    notchMargin: 15,
+    child: Container(
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 10.0,
+              ),
+              MaterialButton(
+                onPressed: () {
+                  setState(() {
+                    currentScreen = DashboardScreen();
+                    currentTab = 0;
+                  });
+                },
+                minWidth: 40,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.home_filled,
+                      color: Colors.amber.shade400,
+                    ),
+                    Text(
+                      "Home",
+                      style: TextStyle(
+                        color: Colors.amber.shade400,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  setState(() {
+                    currentScreen = WorkoutScreen();
+                    currentTab = 1;
+                  });
+                },
+                minWidth: 40,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.timer_rounded,
+                      color: Colors.amber.shade400,
+                    ),
+                    Text(
+                      "Workouts",
+                      style: TextStyle(
+                        color: Colors.amber.shade400,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-          elevation: 2.0,
-        ),
-        bottomNavigationBar: BottomAppBar(
-      shape: CircularNotchedRectangle(),
-      notchMargin: 15,
-      child: Container(
-        height: 70,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 10.0,
-                ),
-                MaterialButton(
-                  onPressed: () {
-                    setState(() {
-                      currentScreen = DashboardScreen();
-                      currentTab = 0;
-                    });
-                  },
-                  minWidth: 40,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.home_filled,
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              MaterialButton(
+                onPressed: () {
+                  setState(() {
+                    currentScreen = NutritionScreen();
+                    currentTab = 2;
+                  });
+                },
+                minWidth: 40,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.food_bank_sharp,
+                      color: Colors.amber.shade400,
+                    ),
+                    Text(
+                      "Nutrition",
+                      style: TextStyle(
                         color: Colors.amber.shade400,
                       ),
-                      Text(
-                        "Home",
-                        style: TextStyle(
-                          color: Colors.amber.shade400,
-                        ),
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 ),
-                MaterialButton(
-                  onPressed: () {
-                    setState(() {
-                      currentScreen = WorkoutScreen();
-                      currentTab = 1;
-                    });
-                  },
-                  minWidth: 40,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.timer_rounded,
+              ),
+              MaterialButton(
+                onPressed: () {
+                  setState(() {
+                    currentScreen = ProfileScreen();
+                    currentTab = 3;
+                  });
+                },
+                minWidth: 40,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.account_circle,
+                      color: Colors.amber.shade400,
+                    ),
+                    Text(
+                      "Profile",
+                      style: TextStyle(
                         color: Colors.amber.shade400,
                       ),
-                      Text(
-                        "Workouts",
-                        style: TextStyle(
-                          color: Colors.amber.shade400,
-                        ),
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                MaterialButton(
-                  onPressed: () {
-                    setState(() {
-                      currentScreen = NutritionScreen();
-                      currentTab = 2;
-                    });
-                  },
-                  minWidth: 40,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.food_bank_sharp,
-                        color: Colors.amber.shade400,
-                      ),
-                      Text(
-                        "Nutrition",
-                        style: TextStyle(
-                          color: Colors.amber.shade400,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                MaterialButton(
-                  onPressed: () {
-                    setState(() {
-                      currentScreen = ProfileScreen();
-                      currentTab = 3;
-                    });
-                  },
-                  minWidth: 40,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.account_circle,
-                        color: Colors.amber.shade400,
-                      ),
-                      Text(
-                        "Profile",
-                        style: TextStyle(
-                          color: Colors.amber.shade400,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: 10.0,
-                )
-              ],
-            )
-          ],
-        ),
+              ),
+              SizedBox(
+                width: 10.0,
+              )
+            ],
+          )
+        ],
       ),
     ),
-      ),
+    ),
     );
   }
 }
